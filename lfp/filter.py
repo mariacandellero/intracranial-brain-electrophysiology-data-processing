d0ca70bc9536c2efc62bcf541f5dfe0085f5c009
mariacandellero <140336585+mariacandellero@users.noreply.github.com>
2023-07-24 11:05:17 +0200

    Add files via upload

---
+++ lfp/filter.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 23 18:32:07 2023
+
+@author: Maria
+"""
+
+
+from scipy.signal import butter, filtfilt
+
+
+def butter_bandpass(lowcut, highcut, fs, order=2):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='bandpass')
+    return b, a
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=2):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = filtfilt(b, a, data)
+    return y
